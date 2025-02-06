cb9d124eda05be31026aa8e946cec02c73d63d2a
jibinjobyp <jibinjoby78@gmail.com>
2025-02-06 12:59:15 -0800

    css updated

--- src/pages/attract.jsx
+++ src/pages/attract.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useRef } from 'react';
+import React, { use, useEffect, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
@@ -8,7 +8,6 @@ import gsap from "gsap";
 
 // Import images
 import pic1 from "../images/rooms/room1.jpeg";
-
 
 const Attract = () => {
   const oneref = useRef(null);
@@ -24,13 +23,13 @@ const Attract = () => {
   const textthreeref = useRef(null);
   const textfourref = useRef(null);
 
-  useEffect(() => { 
+  useEffect(() => {
     gsap.to(oneref.current, {
       duration: 1,
       y: -20,
       opacity: 1,
       ease: "elastic.out",
-      delay:2
+      delay: 2,
     });
 
     gsap.to(tworef.current, {
@@ -49,7 +48,7 @@ const Attract = () => {
       delay: 3,
     });
 
-    gsap.to(fourref.current, { 
+    gsap.to(fourref.current, {
       duration: 1,
       y: -20,
       opacity: 1,
@@ -57,47 +56,52 @@ const Attract = () => {
       delay: 3.5,
     });
 
-    gsap.fromTo(imageoneref.current, 
-      { x: "-100%",opacity: 0 },
-       { x: 0, duration: 1, ease: "bounce",opacity: 1, delay: 0.5 }
-      );
+    gsap.fromTo(
+      imageoneref.current,
+      { x: "-100%", opacity: 0 },
+      { x: 0, duration: 1, ease: "bounce", opacity: 1, delay: 0.5 }
+    );
 
-    gsap.fromTo(imagetworef.current,
+    gsap.fromTo(
+      imagetworef.current,
       { x: "100%", opacity: 0 },
       { x: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 1 }
     );
 
-    gsap.fromTo(imagethreeref.current,
+    gsap.fromTo(
+      imagethreeref.current,
       { x: "-100%", opacity: 0 },
       { x: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 1.5 }
     );
 
-    gsap.fromTo(imagefourref.current,
+    gsap.fromTo(
+      imagefourref.current,
       { x: "100%", opacity: 0 },
       { x: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 2 }
     );
-    gsap.fromTo(textoneref.current,
+    gsap.fromTo(
+      textoneref.current,
       { y: "100%", opacity: 0 },
       { y: 0, duration: 1, ease: "elastic.out", opacity: 1, delay: 0 }
     );
-    gsap.fromTo(textworef.current,
+    gsap.fromTo(
+      textworef.current,
       { y: "100%", opacity: 0 },
       { y: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 0.5 }
     );
 
-    gsap.fromTo(textthreeref.current,
+    gsap.fromTo(
+      textthreeref.current,
       { y: "100%", opacity: 0 },
       { y: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 1 }
     );
 
-    gsap.fromTo(textfourref.current,
+    gsap.fromTo(
+      textfourref.current,
       { y: "100%", opacity: 0 },
       { y: 0, duration: 1, ease: "power3.inOut", opacity: 1, delay: 1.5 }
     );
-
-    
-
-  }, [])
+  }, []);
   return (
     <div className="container">
       {/* First Row */}
@@ -122,25 +126,21 @@ const Attract = () => {
             </SwiperSlide>
           </Swiper>
           <h1 className="room-name room-one" ref={oneref}>
-          Deluxe Cottage
+            Deluxe Cottage
           </h1>
         </div>
-        <div className="text" ref={textoneref}>Cozy and comfortable rooms designed for a relaxing stay.
-
-Amenities: Kitchenette, flat-screen TV, and other modern facilities.
-
-Occupancy: Ideal for couples or small families.
-
-View: Garden view.</div>
+        <div className="text" ref={textoneref}>
+          These palm cottages are designed with natural aesthetics and come with
+          amenities such as a balcony, forest view, and sitting area.
+        </div>
       </div>
 
       {/* Second Row */}
       <div className="row">
-        <div className="text" ref={textworef}>Cozy and comfortable rooms designed for a relaxing stay.
-
-Amenities: Kitchenette, flat-screen TV, and other modern facilities.
-
-Occupancy: Ideal for couples or small families.</div>
+        <div className="text" ref={textworef}>
+          Stone cottages that offer a garden view, balcony, and sitting area,
+          providing a comfortable stay amidst nature.
+        </div>
         <div className="image" ref={imagetworef}>
           <Swiper
             modules={[Autoplay, Pagination]}
@@ -161,7 +161,7 @@ Occupancy: Ideal for couples or small families.</div>
             </SwiperSlide>
           </Swiper>
           <h1 className="room-name room-two" ref={tworef}>
-          Deluxe Cottage
+            Executive Cottages
           </h1>
         </div>
       </div>
@@ -186,41 +186,13 @@ Occupancy: Ideal for couples or small families.</div>
             </SwiperSlide>
           </Swiper>
           <h1 className="room-name room-three" ref={threeref}>
-          Deluxe Cottage          </h1>
+            Family Suites{" "}
+          </h1>
         </div>
-        <div className="text" ref={textthreeref}>Cozy and comfortable rooms designed for a relaxing stay.
-
-Amenities: Kitchenette, flat-screen TV, and other modern facilities.
-
-Occupancy: Ideal for couples or small families.</div>
-      </div>
-      <div className="row">
-        <div className="text" ref={textfourref}>Cozy and comfortable rooms designed for a relaxing stay.
-
-Amenities: Kitchenette, flat-screen TV, and other modern facilities.
-
-Occupancy: Ideal for couples or small families.</div>
-        <div className="image" ref={imagefourref}>
-          <Swiper
-            modules={[Autoplay, Pagination]}
-            spaceBetween={0}
-            slidesPerView={1}
-            autoplay={{ delay: 3000, disableOnInteraction: false }}
-            pagination={{ clickable: true }}
-            loop={true}
-          >
-            <SwiperSlide>
-              <img src={pic1} alt="Slide 1" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={pic1} alt="Slide 2" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={pic1} alt="Slide 3" />
-            </SwiperSlide>
-          </Swiper>
-          <h1 className="room-name room-four" ref={fourref}>
-          Deluxe Cottage          </h1>
+        <div className="text" ref={textthreeref}>
+          Ideal for larger groups or families, these suites feature double
+          bedrooms, a balcony with forest views, a kitchenette, and ample space
+          to accommodate additional guests.
         </div>
       </div>
     </div>
